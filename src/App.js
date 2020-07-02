4a5369a8e12195c96c5c6af5aca2d6be2a8f96b2
gregorysysoev <gregorysysoev@gmail.com>
2020-07-02 16:07:05 +1000

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import Header from "./components/Header"
-import NewService from "./components/NewService/NewService"
+import HotelEntry from "./components/HotelEntry/HotelEntry"
+import Entry from "./components/Entry/Entry"
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 
 export default function App() {
 	return (
-		<div>
-			<Header/>
-			<NewService/>
-		</div>
+		<BrowserRouter>
+        <Switch>
+            <Route path='/login' component={Entry}/>
+            <Route path='/hotel' component={HotelEntry}/>
+        </Switch>
+    </BrowserRouter>
 	)
 }
