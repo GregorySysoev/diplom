b703607e8369edc18cbb92c449ea7fcb980c2e93
Alexander Melnik <colddegree@yandex.ru>
2020-07-03 12:18:38 +1000

    [service] На странице указания доступности сделать добавление и удаление диапазонов

--- src/components/service/TimeRange/TimeRange.js
+++ src/components/service/TimeRange/TimeRange.js
@@ -6,17 +6,45 @@ class TimeRange extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			beginHour: '',
-			beginMinutes: '',
-			endHour: '',
-			endMinutes: '',
+			timeRanges: [
+				{
+					id: 1,
+					beginHour: 0,
+					beginMinutes: 0,
+					endHour: 23,
+					endMinutes: 59,
+				},
+			],
+			lastId: 1,
 		}
 	}
 
-	handleChange = event => {
-		this.setState({
-			[event.target.name]: event.target.value,
-		})
+	handleChange = (id, fieldName, fieldValue) => {
+		this.setState(prevState => {
+			const rangeToChange = prevState.timeRanges.filter(range => range.id === id)[0] || null;
+			rangeToChange[fieldName] = fieldValue;
+			return { timeRanges: prevState.timeRanges };
+		});
+	}
+
+	addRange = () => {
+		this.setState(prevState => ({
+			timeRanges: [
+				...prevState.timeRanges,
+				this._createRange(prevState.lastId + 1, 0, 0, 0, 0),
+			],
+			lastId: prevState.lastId + 1,
+		}));
+	}
+
+	_createRange = (id, beginHour, beginMinutes, endHour, endMinutes) => {
+		return { id, beginHour, beginMinutes, endHour, endMinutes };
+	}
+
+	removeRange = id => {
+		this.setState(prevState => ({
+			timeRanges: prevState.timeRanges.filter(range => range.id !== id),
+		}));
 	}
 
 	render() {
@@ -28,51 +56,82 @@ class TimeRange extends Component {
 				<div>
 					<p className="uk-text-bold">Укажите, в какое время услуга будет доступна</p>
 				</div>
-				<form className="uk-form-horizontal uk-child-width-1-1 uk-child-width-1-2@m">
-					<div data-uk-grid="">
-						<div className="uk-margin">
-							<label className="uk-form-label" htmlFor="begin">С</label>
-							<div id="begin" className="uk-flex">
-								<div className="uk-form-controls uk-flex timeRange-row">
-									<input className="uk-input"
-										   name="beginHour"
-										   onChange={this.handleChange}
-										   type="number"/>
-									<span className="timeRange-label">часов</span>
+				<form className="uk-form-horizontal uk-child-width-1-1 uk-child-width-1-2@s">
+					{this.state.timeRanges.map(range =>
+						<div className="uk-inline" key={range.id}>
+							<fieldset style={{ border: '1px dashed #ccc' }}>
+								<h5>Диапазон</h5>
+								{this.state.timeRanges.length > 1 && (
+									<div className="uk-position-top-right uk-overlay" style={{ padding: '5px' }}>
+										<a uk-icon="trash"
+										   className="uk-icon-button uk-button-danger"
+										   title="Удалить"
+										   onClick={() => this.removeRange(range.id)}/>
+									</div>
+								)}
+								<div data-uk-grid="">
+									<div className="uk-margin">
+										<label className="uk-form-label" htmlFor="begin">С</label>
+										<div id="begin" className="uk-flex">
+											<div className="uk-form-controls uk-flex timeRange-row">
+												<input className="uk-input"
+													   name="beginHour"
+													   value={range.beginHour}
+													   onChange={event => this.handleChange(range.id, event.target.name, +event.target.value)}
+													   type="number"
+													   step={1}
+													   min={0}
+													   max={23}/>
+												<span className="timeRange-label">часов</span>
+											</div>
+											<div className="uk-form-controls uk-flex timeRange-row">
+												<input className="uk-input"
+													   name="beginMinutes"
+													   value={range.beginMinutes}
+													   onChange={event => this.handleChange(range.id, event.target.name, +event.target.value)}
+													   type="number"
+													   step={1}
+													   min={0}
+													   max={59}/>
+												<span className="timeRange-label">минут</span>
+											</div>
+										</div>
+									</div>
 								</div>
-								<div className="uk-form-controls uk-flex timeRange-row">
-									<input className="uk-input"
-										   name="beginMinutes"
-										   onChange={this.handleChange}
-										   type="number"/>
-									<span className="timeRange-label">минут</span>
+								<div data-uk-grid="" style={{ marginTop: 0 }}>
+									<div className="uk-margin" style={{ marginBottom: 0 }}>
+										<label className="uk-form-label" htmlFor="end">До</label>
+										<div id="end" className="uk-flex">
+											<div className="uk-form-controls uk-flex timeRange-row">
+												<input className="uk-input"
+													   name="endHour"
+													   value={range.endHour}
+													   onChange={event => this.handleChange(range.id, event.target.name, +event.target.value)}
+													   type="number"
+													   step={1}
+													   min={0}
+													   max={23}/>
+												<span className="timeRange-label">часов</span>
+											</div>
+											<div className="uk-form-controls uk-flex timeRange-row">
+												<input className="uk-input"
+													   name="endMinutes"
+													   value={range.endMinutes}
+													   onChange={event => this.handleChange(range.id, event.target.name, +event.target.value)}
+													   type="number"
+													   step={1}
+													   min={0}
+													   max={59}/>
+												<span className="timeRange-label">минут</span>
+											</div>
+										</div>
+									</div>
 								</div>
-							</div>
+							</fieldset>
 						</div>
-					</div>
-					<div data-uk-grid="">
-						<div className="uk-margin">
-							<label className="uk-form-label" htmlFor="end">До</label>
-							<div id="end" className="uk-flex">
-								<div className="uk-form-controls uk-flex timeRange-row">
-									<input className="uk-input"
-										   name="endHour"
-										   onChange={this.handleChange}
-										   type="number"/>
-									<span className="timeRange-label">часов</span>
-								</div>
-								<div className="uk-form-controls uk-flex timeRange-row">
-									<input className="uk-input"
-										   name="endMinutes"
-										   onChange={this.handleChange}
-										   type="number"/>
-									<span className="timeRange-label">минут</span>
-								</div>
-							</div>
-						</div>
-					</div>
+					)}
 					<div>
-						<Button type="submit" label="Добавить диапазон"/>
+						<Button type="button" label="Добавить ещё диапазон" onClick={this.addRange}/>
 					</div>
 				</form>
 				<div className="uk-margin-large-top">
