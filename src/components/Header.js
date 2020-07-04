d3c51c3d8bdd0978f968d343cdcf9903888d8534
gregorysysoev <gregorysysoev@gmail.com>
2020-07-05 01:11:50 +1000

    [hotel-management] Спиздил навбар с сокола

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 export default function Header() {
 	return (
-		<div className="uk-navbar-container" data-uk-navbar="">
-			<div className="uk-navbar-left">
-				<a className="uk-navbar-toggle" data-uk-navbar-toggle-icon="" href="#"/>
-			</div>
-		</div>
+		<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+        <nav className="uk-navbar-container" uk-navbar="">
+          <div className="uk-navbar-left">
+			<a className="uk-navbar-toggle" uk-toggle="target: #offcanvas-usage"  data-uk-navbar-toggle-icon="" href="#"/>
+          </div>
+        </nav>
+        <div id="offcanvas-usage" uk-offcanvas="">
+          <div className="uk-offcanvas-bar">
+            <button className="uk-offcanvas-close" type="button" uk-close="" />
+            <ul className="right">
+              <li>
+                <a href="/hotel">Отредактировать информацию о гостинице</a>
+              </li>
+              <li>
+                <a href="/orders">Зарегистрировать прибывших клиентов</a>
+              </li>
+              <li>
+                <a href="/admin/landmarks">Проверить потребность в услугах</a>
+              </li>
+			  <li>
+                <a href="/admin/landmarks">Просмотреть список услуг</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
 	)
 }
